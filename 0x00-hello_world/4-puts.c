3c5f6cc191915ca2f38b472c4b5348b7239ba086
Stanley Osagie Ramos <sagiecyber@gmail.com>
2022-06-30 10:51:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-#include <stdio.h>
 
 /**
  *  * main - Entry point
@@ -12,10 +7,7 @@ int main(void)
  *     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-}
+
