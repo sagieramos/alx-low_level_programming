26d20116508790e1f835438f1aed16bae1a45ba5
Stanley Osagie Ramos <sagiecyber@gmail.com>
2022-07-14 17:03:55 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,27 +6,27 @@
 * @n: int variable.
 * Return: dest.
 */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-    /*Declare and initialize variables*/
-    int i;
-    char *destptr = desst;
-    int len = 0;
-
-    if (dest == 0)
-        return (0);
-
-    /*lenght of string dest*/
-    while (*dest)
-    {
-        len++;
-        dest++;
-    }
-    dest = destptr;
-    for (i =0; i < n && src[i]; i++)
-    {
-        dest(len + i) = src[i];
-    }
-    dest[len + i] = '\0';
-    return (dest);
+	/*Declare and init vars*/
+	int i;
+	char *destPointer = dest;
+	int len = 0;
+	if (dest == 0)
+	{
+		return (0);
+	}
+	/*Lenght of string dest*/
+	while (*dest)
+	{
+		len++;
+		dest++;
+	}
+	dest = destPointer;
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
 }
