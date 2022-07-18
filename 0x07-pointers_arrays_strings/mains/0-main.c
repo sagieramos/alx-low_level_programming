a7d4b06265165e1e11110b5c5574d62e0ef5904d
Stanley Osagie Ramos <sagiecyber@gmail.com>
2022-07-18 20:54:00 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/mains/0-main.c
+++ 0x07-pointers_arrays_strings/mains/0-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *  * simple_print_buffer - prints buffer in hexa
+ * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
  *
@@ -10,37 +10,34 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-        unsigned int i;
+	unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+
+	}
+	printf("\n");
 }
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    char buffer[98] = {0x00};
+	char buffer[98] = {0x00};
 
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);
-    return (0);
+
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
 }
