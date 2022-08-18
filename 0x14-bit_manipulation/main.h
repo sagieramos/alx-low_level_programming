33a083ef016a8026f82ff7e85b364801260184d7
Stanley Osagie Ramos <sagiecyber@gmail.com>
2022-08-18 19:19:54 +0100

    prototype

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
+prints the binary representation of a number.
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
