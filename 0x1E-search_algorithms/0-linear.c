9c489917c10366844f97b7dc1cb79ed622b88646
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-01-28 18:32:58 +0100

    corrected

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 	unsigned int i = 0;
 
 	if (array == NULL)
-		return(-1);
+		return (-1);
 	for (i = 0; i < size; i++)
 		if (*(array + i) == value)
 			return (i);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
 
-#include <unistd.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
