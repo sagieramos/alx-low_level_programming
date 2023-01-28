8295edfc8a1e931259364658a2f452aba7a8b95a
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-01-28 18:30:43 +0100

    corrected

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,8 +13,10 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	unsigned int i = 0;
 
+	if (array == NULL)
+		return(-1);
 	for (i = 0; i < size; i++)
 		if (*(array + i) == value)
 			return (i);
